987ba1fe8ce5b330bf93555d1827680f99a11d3b
Claudiu Cristea <clau.cristea@gmail.com>
2021-04-20 18:06:17 +0300

    Polish

--- scripts/run_tests.php
+++ scripts/run_tests.php
@@ -12,7 +12,7 @@
  */
 
 use Composer\Factory;
-use Composer\IO\BufferIO;
+use Composer\IO\NullIO;
 use Composer\Json\JsonFile;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Filesystem\Filesystem;
@@ -20,11 +20,9 @@ use Symfony\Component\Filesystem\Filesystem;
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 // Main composer object.
-$composer = (new Factory())->createComposer(
-  new BufferIO(),
-  __DIR__ . '/../composer.json'
-);
-// The composer.json handler.
+$io = new NullIO();
+$composer = (new Factory())->createComposer($io, __DIR__ . '/../composer.json');
+// The composer.json file handler.
 $json = new JsonFile(__DIR__ . '/../composer.json');
 
 $args = $_SERVER['argv'];
@@ -39,70 +37,62 @@ if (empty($args)) {
 
 // Read composer.json content.
 $definition = $json->read();
+$gitSplitRepos = $definition['extra']['git-split']['repos'] ?? [];
+if (!$gitSplitRepos) {
+    throw new \LogicException("No repos in composer.json 'extras.git-split'.");
+}
 $devRequires =& $definition['require-dev'];
 $repositories =& $definition['repositories'];
-$gitSplitRepos = $definition['extra']['git-split']['repos'] ?? [];
-
-if (!$gitSplitRepos) {
-    throw new \LogicException(
-      "No repos found in composer.json 'extras.git-split' section."
-    );
-}
 
 // Get the package name for each entry from extras.git-split. The package name
 // is the key, the value is the relative path in monorepo.
-$packages = [];
+$allComponents = [];
 foreach ($gitSplitRepos as $path => $url) {
     $repo = $composer->getRepositoryManager()->createRepository('vcs', [
         'url' => $url,
     ], $path);
     foreach ($repo->getPackages() as $package) {
-        $packages[$package->getName()] = $path;
+        $allComponents[$package->getName()] = $path;
     }
 }
 
 // Validate input.
-foreach ($args as $arg) {
-    if (!isset($packages[$arg])) {
+array_walk($args, function (string $arg) use ($allComponents): void {
+    if (!isset($allComponents[$arg])) {
         throw new InvalidArgumentException("Invalid package '{$arg}'.");
     }
+});
+
+// Get the part from "require-dev" that corresponds to passed sub-repos.
+$selectedDevRequires = array_intersect_key($devRequires, array_flip($args));
+// Get "require-dev" entries that aren't components.
+$nonSubRepoDevRequires = array_diff_key($devRequires, $allComponents);
+// Concatenate passed packages with non-component packages.
+$devRequires = $nonSubRepoDevRequires + $selectedDevRequires;
+// Honour "sort-packages" config.
+if ($composer->getConfig()->get('sort-packages')) {
+    ksort($devRequires);
 }
 
-// Get tha part from "require-dev" that corresponds to passed sub-repos.
-$selectedDevRequires = array_map(function (string $arg) use ($devRequires): string {
-    return $devRequires[$arg];
-}, array_combine($args, $args));
-
-// Isolate all "require-dev" entries that aren't components.
-$nonSubRepoDevRequires = array_diff_key($devRequires, $packages);
-// Concatenate selected with non-selectable from "require-dev". We keep only
-// packages passed at command line together with other dev packages that aren't
-// sub-component.
-$devRequires = $nonSubRepoDevRequires + $selectedDevRequires;
-ksort($devRequires);
-
 // Cleanup "repositories" section.
-$selectedDevRepos = array_intersect_key($packages, $selectedDevRequires);
-$repositories = array_filter(
-  $repositories,
-  function (array $repository) use ($selectedDevRepos): bool {
-      return $repository['type'] === 'path' && in_array($repository['url'], $selectedDevRepos, true);
-  }
-);
+$selectedDevRepos = array_intersect_key($allComponents, $selectedDevRequires);
+$repositories = array_filter($repositories, function (array $repository) use ($selectedDevRepos): bool {
+    return $repository['type'] === 'path' && in_array($repository['url'], $selectedDevRepos, true);
+});
 
 // Finally, remove the packages themselves.
 $fileSystem = new Filesystem();
-array_walk($packages, function (string $path, string $repository) use ($fileSystem, $selectedDevRepos): void {
+array_walk($allComponents, function (string $path, string $repository) use ($fileSystem, $selectedDevRepos): void {
     if (!isset($selectedDevRepos[$repository])) {
         $fileSystem->remove($path);
     }
 });
 
-// Update composer.json
+// Update composer.json.
 $json->write($definition);
 
 // The composer.json file has been changed. Run update.
-passthru("./vendor/bin/composer update");
+passthru("./vendor/bin/composer update --no-interaction --no-progress --ansi");
 
 buildAndRunTests();
 exit(0);
